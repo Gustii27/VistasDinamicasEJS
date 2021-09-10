b8412dfbe3e531e4704b3849fc20e6a2079e646e
Gustii27 <capuozzo.gustavo>
2021-09-09 22:02:00 -0300

    agregando los ultimos cambios

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+const mainController = require("../controllers/main-controller");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get("/", mainController.home);
+router.get("/detalle/:id", mainController.dishDetail);
 
 module.exports = router;
